a86785da71f221c4ccd9e4988d67437bfe279899
sakthivel_annamalai <sakthivel.a@finstein.ai>
2023-07-01 08:51:24 +0530

    status update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -17,6 +17,7 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+      console.log()
     });
   });
 });
